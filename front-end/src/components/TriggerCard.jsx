85d0aad76502d4e740ff0c1c99b6ee42d601ff31
recypher-zz <kniemerg001@me.com>
2025-08-24 09:28:04 -0400

    Added some debugging to TriggerCard

--- front-end/src/components/TriggerCard.jsx
+++ front-end/src/components/TriggerCard.jsx
@@ -7,7 +7,9 @@ function TriggerCard ({ triggerText, replaceText, setText, setReplaceText }) {
                     onClick={(e) => {
                         e.preventDefault();
                         setText(triggerText);
+			console.log(triggerText);
                         setReplaceText(replaceText);
+			console.log(replaceText);
                     }}
                     className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ triggerText }</h5>
